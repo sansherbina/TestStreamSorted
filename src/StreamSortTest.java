0bd4069b1d090d0f7e3960e7f9eaa8371e4cd78c
oshcherbyna <oleksandr.shcherbyna@kenshoo.com>
2021-02-16 17:52:39 +0200

    Initial commit

---
+++ src/StreamSortTest.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class StreamSortTest {
+
+    public static void main(String[] args) {
+        Stream<Integer> s = Arrays.asList(10, 8, 1, 12).stream().map(element -> {
+            System.out.println("Map operation performed");
+            return element + 1;
+        }).sorted(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer t1, Integer t2) {
+                System.out.println("Sorting actually happened");
+                return t1 - t2;
+            }
+        });
+
+        System.out.println("Block of intermediate operations finished");
+
+        Optional<Integer> firstElement = s.findFirst();
+        System.out.println("Terminal operation finished");
+    }
+}
